5985a3fc5d3d2f216b9d9fd449d6e68158f04b61
Travis Leikam <tleikam@ksu.edu>
2023-04-06 00:56:45 +0000

    Release 0.0.1

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"guess","l":"GuessingGame"},{"p":"guess","l":"GuessResult"},{"p":"guess","l":"Main"},{"p":"guess","l":"Renderer"}]
